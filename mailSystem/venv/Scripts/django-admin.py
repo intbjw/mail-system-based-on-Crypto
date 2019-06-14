e5b8d0cba45a5bf9a975f2fa96bdedd30665df99
intBJWWW <a1051393709@qq.com>
2019-06-14 17:40:52 +0800

    建立一个Django框架

---
+++ mailSystem/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\intbjwww\Documents\GitHub\mail-system-based-on-Crypto\mailSystem\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
