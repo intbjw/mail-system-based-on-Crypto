481d0df8f23e35fc10ba0fa02f9676725d0e97f2
intBJWWW <a1051393709@qq.com>
2019-06-14 17:53:18 +0800

    updata

--- mailSystem/login/views.py
+++ mailSystem/login/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    pass
+    return render(request, 'login/index.html')
+
+
+def login(request):
+    pass
+    return render(request, 'login/login.html')
+
+
+def register(request):
+    pass
+    return render(request, 'login/register.html')
+
+
+def logout(request):
+    pass
+    return redirect("/login/")
