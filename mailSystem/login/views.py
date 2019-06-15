4b8815176665d9865afff4cca31ec3663eaef84a
intBJWWW <a1051393709@qq.com>
2019-06-15 21:24:24 +0800

    增加注册功能

--- mailSystem/login/views.py
+++ mailSystem/login/views.py
@@ -4,12 +4,12 @@ from . import models
 from . import forms
 # Create your views here.
 
-
+# 首页
 def index(request):
     pass
     return render(request, 'login/index.html')
 
-
+# 登录视图
 def login(request):
     if request.method == 'POST':
         login_form = forms.UserForm(request.POST)
@@ -35,10 +35,46 @@ def login(request):
     login_form = forms.UserForm()
     return render(request, 'login/login.html', locals())
 
-
+# 注册视图
 def register(request):
-    pass
-    return render(request, 'login/register.html')
+    if request.session.get('is_login', None):
+        return redirect('/index/')
+
+    if request.method == 'POST':
+        register_form = forms.RegisterForm(request.POST)
+        message = "请检查填写的内容！"
+        if register_form.is_valid():
+            username = register_form.cleaned_data.get('username')
+            password1 = register_form.cleaned_data.get('password1')
+            password2 = register_form.cleaned_data.get('password2')
+            email = register_form.cleaned_data.get('email')
+            sex = register_form.cleaned_data.get('sex')
+
+            if password1 != password2:
+                message = '两次输入的密码不同！'
+                return render(request, 'login/register.html', locals())
+            else:
+                same_name_user = models.User.objects.filter(name=username)
+                if same_name_user:
+                    message = '用户名已经存在'
+                    return render(request, 'login/register.html', locals())
+                same_email_user = models.User.objects.filter(email=email)
+                if same_email_user:
+                    message = '该邮箱已经被注册了！'
+                    return render(request, 'login/register.html', locals())
+
+                new_user = models.User()
+                new_user.name = username
+                new_user.password = password1
+                new_user.email = email
+                new_user.sex = sex
+                new_user.save()
+
+                return redirect('/login/')
+        else:
+            return render(request, 'login/register.html', locals())
+    register_form = forms.RegisterForm()
+    return render(request, 'login/register.html', locals())
 
 
 def logout(request):
